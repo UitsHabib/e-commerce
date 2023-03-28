65652f7975cf7970c95eaedca56939d518c71c62
jsKraken <javascriptiqbal@gmail.com>
2023-03-28 10:00:02 +0600

    Initialize Frontend & Backend of E-commerce

---
+++ backend/src/config/lib/express.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const config = require("../index");
+
+module.exports = function () {
+    const app = express();
+    app.use(express.json());
+    app.use(cookieParser("hi there"));
+    app.set("port", process.env["PORT"]);
+
+    const globalConfig = config.getGlobalConfig();
+
+    globalConfig.routes.forEach(function (routePath) {
+        require(path.resolve(routePath))(app);
+    });
+
+    globalConfig.strategies.forEach(function (strategyPath) {
+        require(path.resolve(strategyPath))();
+    });
+
+    return app;
+};
