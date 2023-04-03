41edf577c5fc9dfe615e7b2480732f05b90bfec5
Md Abu Noman <contact.abunoman@gmail.com>
2023-04-03 20:24:07 +0600

    try

--- client/src/common/navbar.component.js
+++ client/src/common/navbar.component.js
@@ -4,25 +4,49 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Logout from "./logout.component";
 
-function Fullnavbar() {
+function Fullnavbar({ user }) {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">e-Commerce</Navbar.Brand>
+                <Navbar.Brand>
+                    <strong>e-Commerce</strong>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link>Home</Nav.Link>
                         <Nav.Link>Products</Nav.Link>
                     </Nav>
+                    <Nav className="me-auto  col-md-6">
+                        <div className="input-group">
+                            <input
+                                type="text"
+                                className="form-control border"
+                                required
+                            />
+                            <span className="input-group-append">
+                                <button
+                                    className="btn btn-outline-secondary bg-white border ms-n5"
+                                    type="button"
+                                >
+                                    <i class="bi bi-search"></i>
+                                </button>
+                            </span>
+                        </div>
+                    </Nav>
                     <Nav>
                         <Nav.Link>
-                            Cart <i className="bi bi-cart-fill"></i>
+                            Cart
+                            <i className="bi bi-cart-fill">
+                                <span class="top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                    9
+                                </span>
+                            </i>
                         </Nav.Link>
                         <NavDropdown
                             title={
                                 <>
-                                    User <i className="bi bi-person-fill"></i>
+                                    {user} <i className="bi bi-person-fill"></i>
                                 </>
                             }
                             id="collasible-nav-dropdown"
