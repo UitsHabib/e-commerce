01a002bf3d40b525d4f29fe4cfb234b1b7cf3b3e
Mamunkha069 <mamunkhan069@gmail.com>
2023-04-09 23:18:51 +0600

    Add Swagger Yaml

--- server/src/modules/user/user.controllers.js
+++ server/src/modules/user/user.controllers.js
@@ -97,12 +97,13 @@ async function login(req, res) {
             process.env.TOKEN_SECRET,
             { expiresIn: "1h", issuer: user.email }
         );
+        delete user.dataValues.password;
 
         res.cookie("access_token", token, {
             httpOnly: true,
         });
 
-        res.status(200).send(token);
+        res.status(200).send(user);
     } catch (err) {
         console.log(err);
         res.status(500).send("Internal server error");
