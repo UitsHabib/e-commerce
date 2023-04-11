c76de9e804acb184be06a88079f72c67cf5fd642
Emon Chowdhury <emonchowdhury942858@gmail.com>
2023-04-11 23:19:08 +0600

    conflict resolve

--- client/src/modules/core/components/sidebar.component.js
+++ client/src/modules/core/components/sidebar.component.js
@@ -1,32 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
     return (
         <>
-            <ul className="navbar-nav sidebar sidebar-dark accordion" style={{'backgroundColor':'#E4E4E7'}} id="accordionSidebar">
-
-                <a className="sidebar-brand d-flex align-items-center text-dark justify-content-center" href="#">
+            <ul
+                className="navbar-nav sidebar sidebar-dark accordion"
+                style={{ backgroundColor: "#E4E4E7" }}
+                id="accordionSidebar"
+            >
+                <a
+                    className="sidebar-brand d-flex align-items-center text-dark justify-content-center"
+                    href="#"
+                >
                     Ecommerce
                 </a>
 
                 <li className="nav-item active">
                     <Link className="nav-link text-dark" to="/">
-                        Dashboard</Link>
+                        Dashboard
+                    </Link>
                 </li>
                 <li className="nav-item active">
                     <Link className="nav-link text-dark" to="/admins">
-                        <span>Admin</span></Link>
+                        <span>Admin</span>
+                    </Link>
                 </li>
                 <li className="nav-item active">
                     <Link className="nav-link text-dark" to="/users">
-                        <span>User</span></Link>
+                        <span>User</span>
+                    </Link>
                 </li>
-               
-
             </ul>
         </>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
